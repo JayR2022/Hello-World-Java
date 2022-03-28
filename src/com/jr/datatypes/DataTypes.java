d8e539d955f206a51edd1b97969799db7acc0f80
Jay.R <jwatson2022@protonmail.com>
2022-03-28 12:56:46 +0100

    JR - feat: datatypes class with associate methods

---
+++ src/com/jr/datatypes/DataTypes.java
@@ -0,0 +1,30 @@
+package com.jr.datatypes;
+
+public class DataTypes {
+	
+	public static void printMessage(String message) {
+		System.out.println(message);
+	}
+	
+	public static String returnString(String message) {
+		return message;
+	}
+	
+	public static boolean returnBoolean() {
+		boolean myStatus = true;
+		
+		return myStatus;
+		
+	}
+	
+	public static float returnMyFloat(int myIntNumber) {
+		float myFloatNumber = myIntNumber;
+		return myFloatNumber;
+	}
+	
+	public static int returnMyChar2Int(char myChar) {
+		 int myInt = myChar;
+		return myInt;
+	}
+
+}
