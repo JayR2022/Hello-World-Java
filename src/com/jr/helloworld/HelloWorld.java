3b6953d80df0d258b53a279f69cf3d707988f5ba
Jay.R <jwatson2022@protonmail.com>
2022-03-28 12:59:13 +0100

    JR: feat importing packages and calling classes from packages

--- src/com/jr/helloworld/HelloWorld.java
+++ src/com/jr/helloworld/HelloWorld.java
@@ -1,20 +1,30 @@
 package com.jr.helloworld;
+import java.util.Scanner;
+
+import com.jr.datatypes.DataTypes;
+
+import java.util.Random;
 
 public class HelloWorld {
 	
-	public static void printMessage(String message) {
-		System.out.println(message);
-	}
-	
-	public static String returnString(String message) {
-		return message;
-	}
-	
 	public static void main(String[] args) {
 		String out_message = "Hello Jay!";
-		System.out.println(returnString(out_message));
-		printMessage("Hello World!");
+		Scanner myScannerObj = new Scanner(System.in);
+		Random myRandom = new Random();
+		
+		System.out.println(DataTypes.returnString(out_message));
+		System.out.println("Make a Selection - (b), (f), (c)");
+		String mySelection = myScannerObj.nextLine();
+		System.out.println("Jay Selected: " + mySelection);
+		
+		System.out.println(String.format("My Random number is: %d", myRandom.nextInt(100)));
+		
+		System.out.println(DataTypes.returnBoolean());
+		System.out.println(DataTypes.returnMyFloat(534));
+		System.out.println(DataTypes.returnMyChar2Int('s'));
+		System.out.println("Hello World!");
 	}
-	
 
 }
+
+
